1060b75196b6f76223625aec7e02843b2ae4de24
Kumar Mithraratne <p.mithraratne@auckland.ac.nz>
2017-12-04 12:31:28 +1300

    Update cellml_split_reaction_diffusion_equation.F90

--- src/fortran/cellml_split_reaction_diffusion_equation.F90
+++ src/fortran/cellml_split_reaction_diffusion_equation.F90
@@ -6,6 +6,150 @@ PROGRAM CELLML_SPLIT_REACTION_DIFFUSION_EQUATION
   USE MPI
 #endif
 
+#ifdef WIN32
+  USE IFQWIN
+#endif
+
+  IMPLICIT NONE
+
+#ifdef NOMPIMOD
+#include "mpif.h"
+#endif
+
+  !-----------------------------------------------------------------------------------------------------------
+  ! PROGRAM VARIABLES AND TYPES
+  !-----------------------------------------------------------------------------------------------------------
+
+  REAL(CMISSRP), PARAMETER :: LENGTH=100.0_CMISSRP
+  
+  INTEGER(CMISSIntg), PARAMETER :: CoordinateSystemUserNumber=1
+  INTEGER(CMISSIntg), PARAMETER :: RegionUserNumber=2
+  INTEGER(CMISSIntg), PARAMETER :: BasisUserNumber=3
+  INTEGER(CMISSIntg), PARAMETER :: GeneratedMeshUserNumber=4
+  INTEGER(CMISSIntg), PARAMETER :: MeshUserNumber=5
+  INTEGER(CMISSIntg), PARAMETER :: DecompositionUserNumber=6
+  INTEGER(CMISSIntg), PARAMETER :: GeometricFieldUserNumber=7
+  INTEGER(CMISSIntg), PARAMETER :: EquationsSetFieldUserNumber=8
+  INTEGER(CMISSIntg), PARAMETER :: DependentFieldUserNumber=9
+  INTEGER(CMISSIntg), PARAMETER :: MaterialsFieldUserNumber=10
+  INTEGER(CMISSIntg), PARAMETER :: EquationsSetUserNumber=11
+  INTEGER(CMISSIntg), PARAMETER :: ProblemUserNumber=12
+  INTEGER(CMISSIntg), PARAMETER :: SourceFieldUserNumber=13
+  INTEGER(CMISSIntg), PARAMETER :: CellMLUserNumber=14
+  INTEGER(CMISSIntg), PARAMETER :: CellMLModelsFieldUserNumber=15
+  INTEGER(CMISSIntg), PARAMETER :: CellMLStateFieldUserNumber=16
+  INTEGER(CMISSIntg), PARAMETER :: CellMLIntermediateFieldUserNumber=17
+  INTEGER(CMISSIntg), PARAMETER :: CellMLParametersFieldUserNumber=18
+
+  !Program types
+  
+  !Program variables
+
+  INTEGER(CMISSIntg) :: NUMBER_GLOBAL_X_ELEMENTS,CONDITION
+  INTEGER(CMISSIntg) :: NUMBER_OF_DOMAINS,NODE_NUMBER,NodeDomain
+  INTEGER(CMISSIntg),DIMENSION(2) :: BCNODES
+  INTEGER(CMISSIntg) :: MPI_IERROR
+  INTEGER :: node
+  REAL(CMISSRP) :: VALUE
+  INTEGER(CMISSIntg) :: constantModelIndex
+
+  !INTEGER(INTG) :: first_global_dof,first_local_dof,first_local_rank,last_global_dof,last_local_dof,last_local_rank,rank_idx
+  !INTEGER(INTG) :: EQUATIONS_SET_INDEX
+  !TYPE(DOMAIN_MAPPING_TYPE), POINTER :: DEPENDENT_DOF_MAPPING
+  
+  !CMISS variables
+
+  TYPE(cmfe_BasisType) :: Basis
+  TYPE(cmfe_CoordinateSystemType) :: CoordinateSystem,WorldCoordinateSystem
+  TYPE(cmfe_DecompositionType) :: Decomposition
+  TYPE(cmfe_EquationsType) :: Equations
+  TYPE(cmfe_EquationsSetType) :: EquationsSet
+  TYPE(cmfe_FieldType) :: GeometricField,EquationsSetField,DependentField,MaterialsField,SourceField
+  TYPE(cmfe_FieldsType) :: Fields
+  TYPE(cmfe_BoundaryConditionsType) :: BoundaryConditions
+  TYPE(cmfe_GeneratedMeshType) :: GeneratedMesh  
+  TYPE(cmfe_MeshType) :: Mesh
+  TYPE(cmfe_ProblemType) :: Problem
+  TYPE(cmfe_ControlLoopType) :: ControlLoop
+  TYPE(cmfe_RegionType) :: Region,WorldRegion
+  TYPE(cmfe_SolverType) :: Solver, LinearSolver
+  TYPE(cmfe_SolverEquationsType) :: SolverEquations
+  TYPE(cmfe_CellMLType) :: CellML
+  TYPE(cmfe_CellMLEquationsType) :: CellMLEquations
+  TYPE(cmfe_FieldType) :: CellMLModelsField,CellMLStateField,CellMLIntermediateField,CellMLParametersField
+
+  LOGICAL :: EXPORT_FIELD
+
+#ifdef WIN32
+  !Quickwin type
+  LOGICAL :: QUICKWIN_STATUS=.FALSE.
+  TYPE(WINDOWCONFIG) :: QUICKWIN_WINDOW_CONFIG
+#endif
+  
+  !Generic CMISS variables
+  
+  INTEGER(CMISSIntg) :: NumberOfComputationalNodes,ComputationalNodeNumber
+  INTEGER(CMISSIntg) :: EquationsSetIndex,CellMLIndex
+  INTEGER(CMISSIntg) :: Err
+
+#ifdef WIN32
+  !Initialise QuickWin
+  QUICKWIN_WINDOW_CONFIG%TITLE="General Output" !Window title
+  QUICKWIN_WINDOW_CONFIG%NUMTEXTROWS=-1 !Max possible number of rows
+  QUICKWIN_WINDOW_CONFIG%MODE=QWIN$SCROLLDOWN
+  !Set the window parameters
+  QUICKWIN_STATUS=SETWINDOWCONFIG(QUICKWIN_WINDOW_CONFIG)
+  !If attempt fails set with system estimated values
+  IF(.NOT.QUICKWIN_STATUS) QUICKWIN_STATUS=SETWINDOWCONFIG(QUICKWIN_WINDOW_CONFIG)
+#endif
+
+  !-----------------------------------------------------------------------------------------------------------
+  ! PROBLEM CONTROL PANEL
+  !-----------------------------------------------------------------------------------------------------------
+
+  !Intialise OpenCMISS
+  CALL cmfe_Initialise(WorldCoordinateSystem,WorldRegion,Err)
+  CALL cmfe_ErrorHandlingModeSet(CMFE_ERRORS_TRAP_ERROR,Err)
+  !Get the computational nodes information
+  CALL cmfe_ComputationalNumberOfNodesGet(NumberOfComputationalNodes,Err)
+  CALL cmfe_ComputationalNodeNumberGet(ComputationalNodeNumber,Err)
+
+  NUMBER_GLOBAL_X_ELEMENTS=10
+  NUMBER_OF_DOMAINS=NumberOfComputationalNodes
+
+  !Set all diganostic levels on for testing
+  CALL MPI_BCAST(NUMBER_GLOBAL_X_ELEMENTS,1,MPI_INTEGER,0,MPI_COMM_WORLD,MPI_IERROR)
+  CALL MPI_BCAST(NUMBER_OF_DOMAINS,1,MPI_INTEGER,0,MPI_COMM_WORLD,MPI_IERROR)
+
+  !-----------------------------------------------------------------------------------------------------------
+  ! COORDINATE SYSTEM
+  !-----------------------------------------------------------------------------------------------------------
+
+  !Start the creation of a new RC coordinate system
+  CALL cmfe_CoordinateSystem_Initialise(CoordinateSystem,Err)
+  CALL cmfe_CoordinateSystem_CreateStart(CoordinateSystemUserNumber,CoordinateSystem,Err)
+  !Set the coordinate system to be 1D
+  CALL cmfe_CoordinateSystem_DimensionSet(CoordinateSystem,1,Err)
+  !Finish the creation of the coordinate system
+  CALL cmfe_CoordinateSystem_CreateFinish(CoordinateSystem,Err)
+
+  !-----------------------------------------------------------------------------------------------------------
+  ! REGION
+  !----------------------------------------------------------------------------------------------------------- 
+
+  !Start the creation of the region
+  CALL cmfe_Region_Initialise(Region,Err)
+  CALL cmfe_Region_CreateStart(RegionUserNumber,WorldRegion,Region,Err)
+  !Set the regions coordinate system to the 1D RC coordinate system that we have created
+  CALL cmfe_Region_CoordinateSystemSet(Region,CoordinateSystem,Err)
+  !Finish the creation of the region
+  CALL cmfe_Region_CreateFinish(Region,Err)
+
+
+
+
+
+
 
 
 
